4cc4390c513af8a27e4bab99955c76d7b4893157
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2025-12-31 06:05:56 -0400

    feat: update TrashView to display development status message

--- frontend/src/views/TrashView.tsx
+++ frontend/src/views/TrashView.tsx
@@ -1,5 +1,8 @@
 export default function TrashView() {
   return (
-    <div>TrashView</div>
-  )
+    <div className="flex flex-col gap-4 mt-40 text-center text-night-muted text-xl">
+      This feat is under development.{" "}
+      <span className="text-night-muted/65 tracking-wider">Coming soon</span>
+    </div>
+  );
 }
