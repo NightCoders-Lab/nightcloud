c8c12137c5d6ea38a62a22273776efdb4fa9fb47
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-01-01 22:32:49 -0400

    fix: update nodeIdsValidator to allow up to 1000 elements in the array

--- backend/src/validators/nodes.validator.ts
+++ backend/src/validators/nodes.validator.ts
@@ -9,8 +9,10 @@ export class NodeValidators {
   ];
   static readonly nodeIdsValidator = [
     body("nodeIds")
-      .isArray({ min: 1, max: 100 })
-      .withMessage("Se debe proporcionar un array de id de nodos válidos"),
+      .isArray({ min: 1, max: 1000 })
+      .withMessage(
+        "Solo puedes seleccionar entre 1 y 1000 elementos a la vez!",
+      ),
     body("nodeIds.*")
       .isUUID(4)
       .withMessage("Cada nodeId debe ser un UUID válido"),
